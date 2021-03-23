8a4c1b13fd0d2b0703aa324dba1b17fb2486af39
Bryan Cartano <bryancartano@outlook.com>
2021-03-23 17:16:04 +0800

    Initial Commit

---
+++ FinalProject/FinalProject/Models/OrderForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+
+namespace FinalProject.Models
+{
+    public class OrderForm
+    {
+        [Key]
+        public int OrderKey { get; set; }
+        [Required(ErrorMessage = "First Name is Required.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is Required.")]
+        [Display(Name = "Last Name")]
+        public  string LastName { get; set; }
+        [Required(ErrorMessage = "Email is Required.")]
+        public string Email { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Address { get; set; }
+        [Display(Name ="Phone Number")]
+        public int PhoneNumber { get; set; }
+        [Display(Name = "Services")]
+        public ServiceType SType { get; set; }
+    }
+
+    public enum ServiceType
+    {
+        Photo = 1,
+        Video = 2
+    }
+}
