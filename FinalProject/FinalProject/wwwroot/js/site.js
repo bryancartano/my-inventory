3c21994a61fd8354f172a6c89b598cd6c69eec17
Bryan Cartano <bryancartano@outlook.com>
2021-03-24 16:47:31 +0800

    Initial Commit

--- FinalProject/FinalProject/wwwroot/js/site.js
+++ FinalProject/FinalProject/wwwroot/js/site.js
@@ -1,25 +1,16 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var modal = document.getElementsByClassName('.simpleModal');
 
-// Write your JavaScript code.
-var modal = document.getElementsByClassName('simpleModal');
-var modalBtn = document.getElementsByClassName('modalBtn');
-var closeBtn = document.getElementsByClassName('closeBtn');
 
-modalBtn.addEventListener('click', openModal);
-closeBtn.addEventListener('click', closeModal);
-window.addEventListener('click', ClickOutside);
+document.querySelectorAll('.modalBtn').forEach(item => {
+    item.addEventListener('click', event => {
+        item.nextElementSibling.style.display = 'block';
 
-function openModal() {
-    modal.style.display = 'block';
-}
+    })
+})
 
-function closeModal() {
-    modal.style.display = 'none';
-}
 
-function ClickOutside(e) {
-    if (e.target == modal) {
-        modal.style.display = 'none';
-    }
-}
+document.querySelectorAll('.closeBtn').forEach(item => {
+    item.addEventListener('click', event => {
+        item.parentElement.parentElement.parentElement.style.display = 'none';
+    })
+})
