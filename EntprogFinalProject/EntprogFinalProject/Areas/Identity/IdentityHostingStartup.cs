5b52a8c40986f4598363cd83176bbd3de0ac9498
Bryan Cartano <bryancartano@outlook.com>
2021-03-31 19:33:52 +0800

    Fixed Initital With BackEnd

---
+++ EntprogFinalProject/EntprogFinalProject/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using EntprogFinalProject.Data;
+using EntprogFinalProject.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EntprogFinalProject.Areas.Identity.IdentityHostingStartup))]
+namespace EntprogFinalProject.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ EntprogFinalProject/EntprogFinalProject/Controllers/OrderController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using EntprogFinalProject.Data;
+using EntprogFinalProject.Models;
+
+namespace EntprogFinalProject.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public OrderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult OrderIndex()
+        {
+            var orderlist = _context.Orders.Include(p => p.Service).ToList();
+            return View(orderlist);
+        }
+        public IActionResult OrderForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult OrderForm(Order forms)
+        {
+            var form = new Order()
+            {
+                Firstname = forms.Firstname,
+                Lastname = forms.Lastname,
+                Email = forms.Email,
+                Address = forms.Address,
+                PhoneNumber = forms.PhoneNumber,
+                Service = forms.Service
+            };
+
+            _context.Orders.Add(form);
+            _context.SaveChanges();
+
+            return RedirectToAction("OrderIndex");
+        }
+
+        public IActionResult EditForm(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("OrderIndex");
+            }
+            var form = _context.Orders.Where(o => o.OrderKey == id).SingleOrDefault();
+            if (form == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(form);
+        }
+
+        [HttpPost]
+        public IActionResult EditForm(int? id, Order forms)
+        {
+            var form = _context.Orders.Where(o => o.OrderKey == id).SingleOrDefault();
+            form.Firstname = forms.Firstname;
+            form.Lastname = forms.Lastname;
+            form.Email = forms.Email;
+            form.Address = forms.Address;
+            form.PhoneNumber = forms.PhoneNumber;
+            form.Service = forms.Service;
+
+            _context.Orders.Update(form);
+            _context.SaveChanges();
+
+            return RedirectToAction("OrderIndex");
+        }
+        public IActionResult DeleteForm(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("OrderIndex");
+            }
+            var form = _context.Orders.Where(o => o.OrderKey == id).SingleOrDefault();
+            if (form == null)
+            {
+                return RedirectToAction("OrderIndex");
+            }
+            _context.Orders.Remove(form);
+            _context.SaveChanges();
+
+            return RedirectToAction("OrderIndex");
+        }
+    }
+}
